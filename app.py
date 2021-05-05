f724464d3c52471500f1411c44a5006c3e39fb6a
Ku-hsun <61910015+Ku-hsun@users.noreply.github.com>
2021-05-05 15:16:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,9 +36,5 @@ def handle_message(event):
     get_message = event.message.text
 
     # Send To Line
-    reply = ImageSendMessage(
-    original_content_url='https://d.ecimg.tw/items/DPAH3IA90096QOY/000001_1530759512.jpg',
-    preview_image_url='https://d.ecimg.tw/items/DPAH3IA90096QOY/000001_1530759512.jpg'
-    )
-    # TextSendMessage(text="你說的是不是"+f"{get_message}" )
+    reply = TextSendMessage(text="是不是要"+f"{get_message}" )
     line_bot_api.reply_message(event.reply_token, reply)
